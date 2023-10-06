9055f44c389493f3c4cf561d3583f125a39d9cd7
Tiwalade <sphinixphase17@gmail.com>
2023-10-05 17:56:04 -0700

    Finish Task 4,5,6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    /* Use the puts function to print the desired message */
+puts("\"Programming is like building a multilingual puzzle");
+
+    /* Return 0 to indicate successful completion of the program */
+return (0);
+}

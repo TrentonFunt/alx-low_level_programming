92f3f89c407834fa15e9c73b14b118a5a8ba2e33
Tiwalade <sphinixphase17@gmail.com>
2023-10-10 17:29:21 -0700

    Finish Task 0-1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints "_putchar" followed by a
+ * new line and returns 0.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-    return (0);
+	return (0);
 }

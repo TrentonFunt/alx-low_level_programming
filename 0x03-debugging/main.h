5d7082a4017bc62189c1653443d93d49fc5dc6ce
Tiwalade <sphinixphase17@gmail.com>
2023-10-11 13:58:06 -0700

    Finish Task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,13 +6,20 @@
 /* Function prototype for positive_or_negative function */
 void positive_or_negative(int i);
 
-#include <stdio.h>
-
 /**
  * main - Entry point for the program
  *
  * Return: Always 0
  */
 int main(void);
+
+/**
+ * main.h - Header file for the largest_number function
+ *
+ * This header file contains the function prototype for largest_number.
+ * It defines the interface for using the largest_number function in other files.
+ */
+
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */

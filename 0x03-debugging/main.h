6fe712be299a600425b617353eec4e8021fb3a03
Tiwalade <sphinixphase17@gmail.com>
2023-10-11 13:31:32 -0700

    Finish Task 1-1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,4 +6,13 @@
 /* Function prototype for positive_or_negative function */
 void positive_or_negative(int i);
 
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: Always 0
+ */
+int main(void);
+
 #endif /* MAIN_H */

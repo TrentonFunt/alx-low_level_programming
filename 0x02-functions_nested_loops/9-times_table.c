6f3884af00d166f9f95885fbed669d2c208cdbb7
Tiwalade <sphinixphase17@gmail.com>
2023-10-10 21:03:57 -0700

    Finish Task 9-1

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,37 @@
 #include "main.h"
 
 /**
- * times_table - Print the times table in the specified format
+ * times_table - Print the 9 times table in the specified format
  */
 void times_table(void)
 {
-	int i, j, result;
+	int row, col, product;
 
-	for (i = 0; i <= 9; i++)
+	for (row = 0; row <= 9; row++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (col = 0; col <= 9; col++)
 		{
-			result = i * j;
-			if (j == 0)
+			product = row * col;
+
+			if (col == 0)
 			{
-				_putchar(result + '0');
+				_putchar('0');
 			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (result < 10)
+
+				if (product < 10)
 				{
 					_putchar(' ');
 				}
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
 			}
+
+			_putchar((product / 10) + '0');
+			_putchar((product % 10) + '0');
 		}
+
 		_putchar('\n');
 	}
 }

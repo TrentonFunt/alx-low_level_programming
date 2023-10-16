ab76d5725e737fb53d19ec6225572a05fbf5d6da
Tiwalade <sphinixphase17@gmail.com>
2023-10-16 16:20:35 -0700

    Finish Task 6-2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string, starting with the fisrt
+ * puts2 - Prints every other character of a string, starting with the first character.
  * @str: The string to be printed.
  */
 void puts2(char *str)

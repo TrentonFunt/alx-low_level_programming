ecf3e33ce42221d4377d9807b1cbd0277e024a72
Tiwalade <sphinixphase17@gmail.com>
2023-10-16 15:01:35 -0700

    Update Files 0,1,2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,21 +10,21 @@ int _putchar(char c);  /* Prototype for the _putchar function */
  * reset_to_98 - Updates the value pointed to by a pointer to 98
  * @n: A pointer to an integer
  */
-void reset_to_98(int *n)
+void reset_to_98(int *n);
 
 /**
  * swap_int - Swaps the values of two integers
  * @a: Pointer to the first integer
  * @b: Pointer to the second integer
  */
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *b);
 
 /**
  * _strlen - Returns the length of a string
  * @s: The string to be measured
  * Return: The length of the string
  */
-int _strlen(char *s)
+int _strlen(char *s);
 
 
 #endif /* MAIN_H */

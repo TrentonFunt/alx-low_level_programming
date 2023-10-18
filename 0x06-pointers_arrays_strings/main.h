3c6beb884d7d100f19351dda540de74108d24fe1
Tiwalade <sphinixphase17@gmail.com>
2023-10-18 12:54:29 -0700

    Finish Task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function that concatenates two strings */
+char *_strcat(char *dest, char *src);
+
+/* Custom putchar function */
+int _putchar(char c);
+
+#endif /* MAIN_H */

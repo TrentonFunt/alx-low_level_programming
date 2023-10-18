fd397aa4f582f20ddd95d909430e57c04ec473ea
Tiwalade <sphinixphase17@gmail.com>
2023-10-18 12:58:29 -0700

    Finish Task 0-1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-        int dest_len = 0, i = 0;
+	int dest_len = 0, i = 0;
 
-        /* Calculate the length of dest */
-        while (dest[dest_len] != '\0')
-        {
-                dest_len++;
-        }
+	/* Calculate the length of dest */
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
 
-        /* Append src to dest */
-        while (src[i] != '\0')
-        {
-                dest[dest_len + i] = src[i];
-                i++;
-        }
+	/* Append src to dest */
+	while (src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
 
-        /* Add a terminating null byte */
-        dest[dest_len + i] = '\0';
+	/* Add a terminating null byte */
+	dest[dest_len + i] = '\0';
 
-        return dest;
+	return (dest);
 }

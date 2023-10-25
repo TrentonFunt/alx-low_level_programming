5aebae5f3385b63fb2d1e2dd147228325f4254e7
Tiwalade <sphinixphase17@gmail.com>
2023-10-25 14:19:29 -0700

    Finish Task 0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prototype for the _putchar function */
+int _putchar(char c);
+
+/* Prototype for the _puts_recursion function */
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */

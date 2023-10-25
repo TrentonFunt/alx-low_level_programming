c7744447013183956ea3e57143558ad25e0971ee
Tiwalade <sphinixphase17@gmail.com>
2023-10-25 14:41:20 -0700

    Finish Task 1,2,3

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Prototype for the factorial function */
+int factorial(int n);
+
+/* Prototype for the _strlen_recursion function */
+int _strlen_recursion(char *s);
+
+/* Prototype for the _print_rev_recursion function */
+void _print_rev_recursion(char *s);
+
 /* Prototype for the _putchar function */
 int _putchar(char c);
 

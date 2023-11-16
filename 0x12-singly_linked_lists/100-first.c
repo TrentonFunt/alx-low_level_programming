e5b6b9eac3ede7760c1bd998cf8432acd5a333b9
TrentonFunt <sphinixphase17@gmail.com>
2023-11-16 23:35:32 +0100

    Correct Errors on 100-First again

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
  */
 void startup_text(void)
 {
-	printf(
-	       "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
-		);
+printf(
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+);
 }

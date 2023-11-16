57fc304cb13baf899bf9b443d5e1dea8eebc3485
TrentonFunt <sphinixphase17@gmail.com>
2023-11-16 22:31:38 +0100

    Add node at beginning and end

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h> /* for size_t */
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +23,6 @@ typedef struct list_s
 /* Function prototype for print list */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 #endif /* LISTS_H */

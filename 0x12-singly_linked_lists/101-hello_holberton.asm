7b5fcb63483adb5e370bc2db1c2a0fd66191602d
TrentonFunt <sphinixphase17@gmail.com>
2023-11-16 23:28:45 +0100

    Complete Advanced Tasks

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+SECTION .data
+    greeting: db "Hello, Holberton", 0
+    my_fmt:   db "%s", 10, 0
+
+SECTION .text
+    extern printf
+    global main
+
+main:
+    ; Set up arguments for printf
+    mov esi, greeting ; pointer to the string
+    mov edi, my_fmt   ; pointer to the format string
+    mov eax, 0        ; format specifier (0 corresponds to %s)
+
+    ; Call printf
+    call printf
+
+    ; Exit the program
+    mov eax, 0
+    ret

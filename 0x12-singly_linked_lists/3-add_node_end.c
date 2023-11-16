562907ae78b10f8a0bf4bb6a70ff8eb6de6a6208
TrentonFunt <sphinixphase17@gmail.com>
2023-11-16 23:00:29 +0100

    Correction

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * add_node_end - adds a new node at the end of a list_t list
@@ -10,38 +10,38 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new_node, *l_node;
-unsigned int len = 0;
+	list_t *new_node, *end_node;
+	unsigned int len = 0;
 
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-	return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return (NULL);
-}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-while (str[len])
-	len++;
+	while (str[len])
+		len++;
 
-new_node->len = len;
-new_node->next = NULL;
+	new_node->len = len;
+	new_node->next = NULL;
 
-if (*head == NULL)
-{
-*head = new_node;
-}
-else
-{
-l_node = *head;
-while (l_node->next != NULL)
-	l_node = last_node->next;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		end_node = *head;
+		while (end_node->next != NULL)
+			end_node = last_node->next;
 
-l_node->next = new_node;
-}
+		end_node->next = new_node;
+	}
 
-return (new_node);
+	return (new_node);
 }

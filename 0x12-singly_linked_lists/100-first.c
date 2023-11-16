41182923086c8e3a5fd521e9337a9b9daa4ec7ec
TrentonFunt <sphinixphase17@gmail.com>
2023-11-16 23:47:42 +0100

    correct errors

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * startup_text - prints a startup message
+ * startup_text - executed before the main function
  */
-void startup_text(void)
+void __attribute__((constructor)) startup_text(void)
 {
 printf(
 "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
